28d8c005958fe787802888839b8aab48771aac6c
Caio Silva <caiodepaula.silva@gmail.com>
2024-04-10 00:22:43 -0300

    removing: unused content

--- Infrastructure/Database/SQLDBContext.cs
+++ Infrastructure/Database/SQLDBContext.cs
@@ -9,20 +9,6 @@ public class SQLDBContext(DbContextOptions<SQLDBContext> options) : DbContext(op
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfiguration(new ProductEntityTypeConfiguration());
-
-        //modelBuilder.Seed();
-    }
-}
-
-public static class ModelBuilderExtensions
-{
-    public static void Seed(this ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Product>().HasData(
-            new Product { Id = 1, Nome = "Enfermedad" },
-            new Product { Id = 2, Nome = "Diligencias" },
-            new Product { Id = 3, Nome = "Otros" }
-            );
+        modelBuilder.ApplyConfiguration(new ProductEntityTypeConfiguration());        
     }
 }
--- Spike-CasasBahia/Controllers/ProductController.cs
+++ Spike-CasasBahia/Controllers/ProductController.cs
@@ -1,44 +1,46 @@
+using Application.Services;
 using Domain.DTOs;
 using Domain.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Spike_CasasBahia.Controllers
 {
     [ApiController]
+    [AllowAnonymous]
     [Route("api/v1/[controller]")]
-    public class ProductController(ILogger<ProductController> logger, IProductService productService) : ControllerBase
-    {
-        private readonly ILogger<ProductController> _logger = logger;
+    public class ProductController(IProductService productService, IKafkaService kafkaService) : ControllerBase
+    {        
         private readonly IProductService _productService = productService;
+        private readonly IKafkaService _kafkaService = kafkaService;
 
-        [HttpPost]
+        [HttpPost]        
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Post([FromBody] ProductDto product)
+        public async Task<IActionResult> Post([FromBody] ProductDto productDto)
         {
-            var result = await _productService.AddProduct(product);
-            _logger.LogInformation(result.ToString());
-            return Ok(result);
+            var product = await _productService.AddProduct(productDto);            
+            await _kafkaService.ProduceAsync("Estoque", product);
+
+            return Created(nameof(Get), product);
         }
 
-        [HttpGet("{Id}")]
+        [HttpGet("id")]        
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> Get([FromBody] int id)
+        public async Task<IActionResult> Get([FromQuery] int id)
         {
-            var result = await _productService.GetProduct(id);
-            _logger.LogInformation(result.ToString());
+            var result = await _productService.GetProduct(id);            
             return Ok(result);
         }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> Get([FromBody] ProductDto product)
+        public async Task<IActionResult> Get()
         {
-            var result = await _productService.GetProducts();
-            _logger.LogInformation(result.ToString());
+            var result = await _productService.GetProducts();            
             return Ok(result);
         }
     }
